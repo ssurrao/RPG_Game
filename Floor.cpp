6e0996e4d12cf2c9923cf998d7a8a921df485bc2
mthian <35182352+mthian@users.noreply.github.com>
2018-01-07 00:19:32 -0800

    Update Floor.cpp

--- Floor.cpp
+++ Floor.cpp
@@ -5,6 +5,7 @@ using namespace std;
 //Constructor for Floor
 //for file variable. An ifstream variable must already be created with corresponding floor file.
 Floor::Floor(const int height, const int width, ifstream& file): height(height), width(width), floor(new Cell*[height]) {
+    //This 2D array is not working, wont initialize a Array but instead thinks we are trying to initialize class
     for(int i = 0; i < height; ++i) {
         floor[i] = new Cell[width];
     }
